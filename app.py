0c1d657bd6344a6145b33c2c97c802d567d29124
msamedozmen <msamedozmen@gmail.com>
2024-03-15 21:38:59 +0300

    Ready to Publish

--- app.py
+++ app.py
@@ -9,59 +9,14 @@ from matplotlib.ticker import FuncFormatter
 
 import plotly.graph_objects as go
 import plotly.express as px
+merged_df = pd.read_excel("merged.xlsx")
+
+
 
 st.set_page_config(page_title="KD 3198 Stats Dashboard",layout="wide")
 
-
-@st.cache_data
-def get_data_from_excel():
-    df = pd.read_csv( "18Jan_3199.csv"
-    )
-    return df 
-
-
-# st.markdown("<p style='color: yellow; font-family: Tahoma, Verdana, sans-serif; font-weight: bold; margin-left: 10px;'>Kingdom 3198 Data & Stats Tracking</p>", unsafe_allow_html=True)
-
-df = get_data_from_excel()
-print(df.columns)
-
-
-df["T4-Kills"] = df["T4-Kills"].str.replace('.', '',regex=True).astype('int64')  
-df["T5-Kills"] = df["T5-Kills"].str.replace('.', '',regex=True).astype('int64')
-df["Kill Points"] = df["Kill Points"].str.replace('.', '',regex=True).astype('int64')
-df["Governor ID"] = df["Governor ID"].astype(str).str.replace('.', '',regex=True)
-df["Power"] = df["Power"].str.replace('.', '',regex=True).astype('int64')
-
-pd.options.display.large_repr = 'truncate'
-
-total_t4Kills =  df["T4-Kills"].sum()
-total_t5Kills =  df["T5-Kills"].sum()
-total_kills =  total_t4Kills+total_t5Kills
-total_deads =  df["T4-Kills"].sum()
-total_t4Kills1 =  df["T4-Kills"].sum()*2
-total_t5Kills1=  df["T5-Kills"].sum()*2
-total_t4Kills2 =  df["T4-Kills"].sum()*3
-total_t5Kills2 =  df["T5-Kills"].sum()*3
-total_t4Kills3 =  df["T4-Kills"].sum()*4
-total_t5Kills3 =  df["T5-Kills"].sum()*4
-data = {'T4 Kills': [total_t4Kills,total_t4Kills1,total_t4Kills2,total_t4Kills3],
-        'T5 Kills':[total_t5Kills,total_t5Kills1,total_t5Kills2,total_t5Kills3]}
-
-new_df = pd.DataFrame(data)
-# Sort DataFrame
-df = df.sort_values(by=["Kill Points"], ascending=False)
-
-df.index = range(1, len(df) + 1)
-df = df.rename_axis("Rank")
-
-
-
-header_col1, header_col2, header_col3, header_col4 = st.columns(4)
-
-
 def format_tick_label(x):
     if abs(x) >= 1e9:
-        print(x)
         return f"{x / 1e9:.2f}B"
     elif abs(x) >= 1e6:
         return f"{x / 1e6:.2f}M"
@@ -70,14 +25,99 @@ def format_tick_label(x):
 
 
 
-# Render title in the first column
+@st.cache_data
+def get_data_from_excel(option):
+    if option == "All Kvk":
+        df = pd.read_excel("merged.xlsx")
+    else:
+        option = option.lower()
+        df = pd.read_excel(f"{option}.xlsx")
+    return df 
+
+
+def get_df_from_name(name):
+
+    if name != "All Names":
+        show_df =  df_grouped.where(df_grouped["Governor Name"]==name).dropna()
+        pie_df = merged_df.where(merged_df["Governor Name"]==name).dropna()
+        
+        return show_df,pie_df
+    
+def get_df_from_id(id):
+    if id != "All ID":
+        show_df =  df_grouped.where(df_grouped["Governor ID"].astype(str)==id).dropna()
+        pie_df = merged_df.where(merged_df["Governor ID"].astype(str)==id).dropna()
+
+    else:
+        show_df = df_grouped
+        pie_df = merged_df
+    return show_df,pie_df
+
+st.markdown("""
+            <style> .st-af {
+                font-size:20px;
+            }
+            p, ol, ul, dl {
+                font-size:22px; }
+            .st-emotion-cache-1avcm0n .ezrtsby2 {
+                visibility:hidden;
+            }
+            </style>
+            """,unsafe_allow_html=True)
+
+
+pd.options.display.large_repr = 'truncate'
+
+
+
+
+header_col1, header_col2, header_col3, header_col4 = st.columns(4)
+
+kvk_list = ["All Kvk","KvK1", "KvK2", "KvK3"]
+
 title = header_col1.markdown("<p style='font-size :48px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold; margin-left: 10px;'>Kingdom 3198</p>", unsafe_allow_html=True)
 
-# Render select boxes in the remaining columns
-kvk = header_col2.selectbox("Kvk", options=["All Kvk","KvK1", "KvK2", "KvK3"])
-id_ = header_col3.selectbox("ID", options=["All ID"])
-name = header_col4.selectbox("NAME", options=["All Names"])
+kvk = header_col2.selectbox("Kvk", options=kvk_list)
+df = get_data_from_excel(kvk)
+try : 
+    df=df.drop(columns=["Unnamed: 0"])
+except:
+    pass
+
+df["Governor ID"] = df["Governor ID"].astype(str)
+
+df_grouped = df.groupby(["Governor ID", "Governor Name"]).sum()
+
+df_grouped.reset_index(inplace=True)
+
+id_options = df_grouped["Governor ID"].tolist()
+name_options = df_grouped["Governor Name"].tolist()
+id_options.insert(0, "All ID")
+name_options.insert(0, "All Names")
+
+df_grouped = df_grouped.sort_values(by="Total Kill Points",ascending=False).reset_index()   
+
+df_grouped=df_grouped.dropna()
+df_grouped = df_grouped.drop(columns="index")
+
+total_t4Kills = df_grouped["T4 Kills"].sum()
+total_t5Kills = df_grouped["T5 Kills"].sum()
+total_kills = df_grouped["T5-T4 Total Kills"].sum()
+total_deads = df_grouped["Dead Gain"].sum()
+
+table_df = merged_df.groupby(["KvK"],as_index=False).sum()
+table_df = table_df.drop(columns=["Governor ID","T4 Kills","T5 Kills","Total Kill Points"])
+id = header_col3.selectbox("ID", options=id_options,key="ID")
+name = header_col4.selectbox("NAME", options=name_options,)
 sub_title = header_col1.markdown("<p style='font-size :32px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold; margin-left: 10px;'>Data & Stats Tracking</p>", unsafe_allow_html=True)
+
+
+
+if name != "All Names":
+    show_df,pie_df = get_df_from_name(name)
+else:
+    show_df,pie_df = get_df_from_id(id)
+
 
 col1, col2, col3 = st.columns(spec=[0.25,0.25,0.5],gap="large")
 
@@ -85,7 +125,7 @@ with col1:
     st.markdown("<p style='font-size :20px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold; margin-left: 10px;'>Performance Breakdown</p>", unsafe_allow_html=True)
     st.markdown("<p style='font-size :16px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold; margin-top:0;margin-bottom:5.5px ;margin-left: 10px;'>by Total Kills, T5 & T4 Kills, Dead Troops</p>", unsafe_allow_html=True)
 
-    st.header("Totals")
+    st.header("Seperated T4 - T5 Kills")
     st.subheader("Total T4 Kills")
     st.info(f"{format_tick_label(total_t4Kills)}")
 
@@ -96,113 +136,66 @@ with col2:
     st.markdown("<p style='font-size :20px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold;visibility:hidden ;margin-left: 10px;'>Performance Breakdown</p>", unsafe_allow_html=True)
     st.markdown("<p style='font-size :16px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold;visibility:hidden; margin-top:0;margin-bottom:5.5px ;margin-left: 10px;'>by Total Kills, T5 & T4 Kills, Dead Troops</p>", unsafe_allow_html=True)
 
-    st.header("Totals")
+    st.header("Total Kill - Dead")
     st.subheader("Total Kill Points")
     st.info(f"{format_tick_label(total_kills)}")
 
     st.subheader("Total Deads")
     st.info(f"{format_tick_label(total_deads)}")
 
+
+
 with col3:
     st.markdown("<p style='font-size :20px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold;visibility:hidden ;margin-left: 10px;'>Performance Breakdown</p>", unsafe_allow_html=True)
     st.markdown("<p style='font-size :16px; color: yellow; margin-right:20px; font-family: Tahoma, Verdana, sans-serif; font-weight: bold;visibility:hidden; margin-top:0;margin-bottom:5.5px ;margin-left: 10px;'>by Total Kills, T5 & T4 Kills, Dead Troops</p>", unsafe_allow_html=True)
+    fig = px.scatter(table_df, x="T5-T4 Total Kills", y="Dead Gain", color="KvK", 
+                    title=" KvK Performance",
+                    labels={"T5-T4 Total Kills": "Total Kills ", "Dead Gain": "Total Deads ", "KvK ": "KvK"},
+                    template="plotly_dark")
 
-    fig = px.scatter(new_df,x="T4 Kills", y="T5 Kills", size =[30]*4
-                    )
-    fig.update_layout(
-    title="Pilot Scatter Plot",
-    xaxis=dict(
-        title='Total T4 Kills',
-        tickfont=dict(size=12, color='yellow'),  # Customize tick font size and color
-        gridcolor='black',  # Change grid color
-        showline=True,  # Show x-axis line
-        linewidth=2,  # Set x-axis line width
-        linecolor='orange',  # Set x-axis line color
-        # tickvals=x,  # Set tick values
-        # ticktext=[format_tick_label(total_t4Kills)],  # Set custom tick labels
+    fig.update_traces(marker=dict(size=30, opacity=0.7))
 
-        
-    ),
-    yaxis=dict(
-        title='Total T5 Kills',
-        tickfont=dict(size=12, color='orange'),  # Customize tick font size and color
-        gridcolor='black',  # Change grid color
-        showline=True,  # Show y-axis line
-        linewidth=2,  # Set y-axis line width
-        linecolor='orange',
-        # tickvals=[total_t5Kills],  # Set tick values
+    fig.update_layout(xaxis=dict(
+                        title='Total Kills',
+                        tickfont=dict(size=12, color='yellow'),  
+                        gridcolor='black',  
+                        showline=True,  
+                        linewidth=2, 
+                        linecolor='orange'), 
+                    yaxis=dict(
+                        title='Total Deads',
+                        tickfont=dict(size=12, color='orange'),  
+                        gridcolor='black',  
+                        showline=True, 
+                        linewidth=2, 
+                        linecolor='orange'), 
+                    plot_bgcolor='black', 
+                    font=dict(family="Arial", size=14, color="orange"))  
 
-        # ticktext=[format_tick_label(total_t5Kills)],  # Set custom tick labels
-# Set y-axis line color
-    ),
-    plot_bgcolor='black',  # Set plot background color
-    font=dict(
-        family="Arial",  # Set font family
-        size=14,  # Set font size
-        color="orange"  # Set font color
-    ),
-
-
-)
-
-
-
-    # Show plot
+    # Display the scatter plot
     st.plotly_chart(fig,use_container_width=True)
-col4,col5 = st.columns([0.5,0.5])
+col4,col5,col6 = st.columns([0.6,0.3,0.3])
 with col4:
-    st.dataframe(df,use_container_width=True)
+    st.dataframe(show_df,use_container_width=True)
+
+with col5:
+    fig_t5_t4 = px.pie(pie_df, values='T5-T4 Total Kills', names='KvK', title='T5-T4 Kill Distribution on KvKs')
+    fig_t5_t4.update_traces(marker=dict(colors=['#1f77b4', '#ff7f0e', '#2ca02c'], line=dict(color='#000000', width=2)))
+    fig_t5_t4.update_layout(template='plotly_dark')
+
+    st.plotly_chart(fig_t5_t4,use_container_width=True)
+with col6:
+    fig_dead = px.pie(pie_df, values='Dead Gain', names='KvK', title='Dead Distribution on KvKs')
+    fig_dead.update_traces(marker=dict(colors=['#1f77b4', '#ff7f0e', '#2ca02c'], line=dict(color='#000000', width=2)))
+    fig_dead.update_layout(template='plotly_dark')
+
+    st.plotly_chart(fig_dead,use_container_width=True)
 
 def format_tick_label(x):
     if abs(x) >= 1e9:
-        print(x)
         return f"{x / 1e9:.2f}B"
     elif abs(x) >= 1e6:
         return f"{x / 1e6:.2f}M"
     else:
         return str(x)
 
-
-
-# fig, ax = plt.subplots()
-# # Create pie chart using Plotly
-# fig = go.Figure(data=[go.Pie(labels=df['Governor ID'], values=df['Kill Points'], hoverinfo='label+percent', textinfo='none')])
-
-# # Set layout options
-# fig.update_layout(title="Kill Points Distribution")
-
-# # Display the pie chart
-# st.plotly_chart(fig)
-
-
-st.markdown("""
-            <style> .st-af {
-                font-size:20px;
-            }
-            p, ol, ul, dl {
-                font-size:22px; }
-            </style>
-            """,unsafe_allow_html=True)
-
-# # Sample data
-# data = {
-#     "Name": ["John", "Alice", "Bob", "Emily", "John", "Alice", "Bob", "Emily"],
-#     "KillPoints": [1000000000, 2000000000, 3000000000, 4000000000, 1500000000, 2500000000, 3500000000, 4500000000],
-#     "Day": ["Monday", "Monday", "Monday", "Monday", "Tuesday", "Tuesday", "Tuesday", "Tuesday"]
-# }
-
-# # Create a DataFrame from the sample data
-# df = pd.DataFrame(data)
-
-# # Render the DataFrame
-# st.dataframe(df)
-
-# # Aggregate the kill points for each person across all days
-# df_agg = df.groupby('Name')['KillPoints'].sum().reset_index()
-
-# # Create a pie chart for the aggregated data
-# fig = go.Figure(data=[go.Pie(labels=df_agg['Name'], values=df_agg['KillPoints'])])
-# fig.update_layout(title="Total Kill Points Distribution")
-
-# # Display the pie chart
-# st.plotly_chart(fig)
